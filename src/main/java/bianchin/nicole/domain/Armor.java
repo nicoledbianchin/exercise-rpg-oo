fbb9e8e4d46fb133d3bf9540566a7d87160d3b75
nicoledbianchin <nickdiasbianchin@hotmail.com>
2020-01-22 10:14:49 -0300

    constroi classe armadura

--- src/main/java/bianchin/nicole/domain/Armor.java
+++ src/main/java/bianchin/nicole/domain/Armor.java
@@ -1,4 +1,13 @@
 package main.java.bianchin.nicole.domain;
 
 public class Armor {
+
+    private String name;
+    private Points defensePoints;
+
+    public Armor(String name, Points defensePoints){
+        this.name = name;
+        this.defensePoints = defensePoints;
+    }
+
 }
