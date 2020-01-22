4d702d3029c7486e25956448cd8d908e220dfdb1
nicoledbianchin <nickdiasbianchin@hotmail.com>
2020-01-22 10:33:04 -0300

    cria classe de personagem

--- src/main/java/bianchin/nicole/domain/Character.java
+++ src/main/java/bianchin/nicole/domain/Character.java
@@ -1,4 +1,17 @@
 package main.java.bianchin.nicole.domain;
 
 public class Character {
+
+    private String name;
+    private Points points;
+    private Weapon weapon;
+    private Armor armor;
+
+    public Character (String name, Points points, Weapon weapon, Armor armor){
+        this.name = name;
+        this.points = points;
+        this.weapon = weapon;
+        this.armor = armor;
+    }
+
 }
