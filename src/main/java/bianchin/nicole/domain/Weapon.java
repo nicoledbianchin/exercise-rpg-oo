55aab6d5539618cd80bf4765e506b8247caf723c
nicoledbianchin <nickdiasbianchin@hotmail.com>
2020-01-22 10:17:24 -0300

    constroi classe arma

--- src/main/java/bianchin/nicole/domain/Weapon.java
+++ src/main/java/bianchin/nicole/domain/Weapon.java
@@ -1,4 +1,13 @@
 package main.java.bianchin.nicole.domain;
 
 public class Weapon {
+
+    private String name;
+    private Points attackPoints;
+
+    public Weapon(String name, Points attackPoints){
+        this.name = name;
+        this.attackPoints = attackPoints;
+    }
+
 }
