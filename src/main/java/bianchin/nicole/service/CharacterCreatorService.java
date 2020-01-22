193461c05aeae89f15421b5fc06f46d2902f42ea
nicoledbianchin <nickdiasbianchin@hotmail.com>
2020-01-22 11:16:46 -0300

    cria metodo de formatar saida

--- src/main/java/bianchin/nicole/service/CharacterCreatorService.java
+++ src/main/java/bianchin/nicole/service/CharacterCreatorService.java
@@ -7,22 +7,25 @@ import main.java.bianchin.nicole.domain.Weapon;
 
 public class CharacterCreatorService {
 
-    public Characters createDefenseCharacter(){
+    public String createDefenseCharacter(){
         Points characterPoints = new Points(50, 30, 2, 10);
         Weapon weapon = new Weapon("Faca de cozinha enferrujada", 30);
         Armor armor = new Armor("Armadura de Couro de Capivara", 50);
         Characters defenseCharacter = new Characters("Gaete, o monstro", characterPoints, weapon, armor);
-        return defenseCharacter;
+        return formatOutput(defenseCharacter);
     }
 
-    public Characters createAttackCharacter(){
+    public String createAttackCharacter(){
         Points characterPoints = new Points(50, 30, 2, 10);
         Weapon weapon = new Weapon("Faca de cozinha enferrujada", 30);
         Armor armor = new Armor("Armadura de Couro de Capivara", 50);
         Characters attackCharacter = new Characters("ROmilda, a destruidora", characterPoints, weapon, armor);
-        return attackCharacter;
+        return formatOutput(attackCharacter);
     }
 
-
+    public String formatOutput(Characters character){
+        String result = character.getName() + "/nForça: " + character.getPoints().getForcePoints() + "/nAgilidade: " + character.getPoints().getAgilityPoints() + "/nCarisma: " + character.getPoints().getCharismaPoints() + "/nConcentração: " + character.getPoints().getConcentrationPoints() + "/nArma: " + character.getWeapon() + "/nArmadura: " + character.getArmor();
+        return result;
+    }
 
 }
