64f810e39d601a9d7d981757154166a7483b0183
nicoledbianchin <nickdiasbianchin@hotmail.com>
2020-01-22 11:31:03 -0300

    faz ajustes finais

--- src/main/java/bianchin/nicole/Application.java
+++ src/main/java/bianchin/nicole/Application.java
@@ -1,4 +1,26 @@
 package main.java.bianchin.nicole;
 
+import main.java.bianchin.nicole.service.CharacterCreatorService;
+
+import java.util.Scanner;
+
 public class Application {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        CharacterCreatorService characterCreatorService = new CharacterCreatorService();
+
+        System.out.println("Escolha o tipo de personagem: ");
+        System.out.println("1 - Personagem de defesa");
+        System.out.println("2 - Personagem de ataque");
+
+        int entrada = scanner.nextInt();
+
+        if (entrada == 1){
+            System.out.println(characterCreatorService.createDefenseCharacter());
+        } else {
+            System.out.println(characterCreatorService.createAttackCharacter());
+        }
+    }
+
 }
