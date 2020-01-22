9edbeec2e166b5a0bd7221ab9cc9bde6fcd62e0e
nicoledbianchin <nickdiasbianchin@hotmail.com>
2020-01-22 09:58:08 -0300

    constroi classe de pontos

---
+++ src/main/java/bianchin/nicole/domain/Armor.java
@@ -0,0 +1,4 @@
+package main.java.bianchin.nicole.domain;
+
+public class Armor {
+}
---
+++ src/main/java/bianchin/nicole/domain/Points.java
@@ -0,0 +1,17 @@
+package main.java.bianchin.nicole.domain;
+
+public class Points {
+
+    private int forcePoints;
+    private int agilityPoints;
+    private int charismaPoints;
+    private int concentrationPoints;
+
+    public Points(int forcePoints, int agilityPoints, int charismaPoints, int concentrationPoints){
+        this.forcePoints = forcePoints;
+        this.agilityPoints = agilityPoints;
+        this.charismaPoints = charismaPoints;
+        this.concentrationPoints = concentrationPoints;
+    }
+
+}
